92800f7f7e67fdacee5a4bcd06dac7d8d2832ec6
Charles <adeakinyemi129@gmail.com>
2025-02-21 22:44:48 +0100

    implemented delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,9 @@ cart.forEach((cartItem)=> {
     })
 
     itemHtml  += `
-        <div class="cart-item-container">
+        <div class="cart-item-container 
+            js-cart-item-container-${matchingProduct.id}
+        ">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -106,7 +108,10 @@ document.querySelectorAll('.delete-quantity-link').forEach((link)=>{
         const productId = link.dataset.productId
     
         removeFromCart(productId)
-        console.log(cart)  
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
+
+        container.remove()
         
     })
 })
