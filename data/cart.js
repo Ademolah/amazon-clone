4f03332a91f3b5f3cfaa1b4c2a7bec3414608289
Charles <adeakinyemi129@gmail.com>
2025-02-21 19:20:30 +0100

    added checkout script

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,12 @@
-export const cart = [];
+export const cart = [{
+  productId: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+  quantity: 6
+},
+{
+  productId: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+  quantity: 2
+}
+];
 
 export function addToCart(productId, value){
       let matchingItem;
